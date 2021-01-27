65edc7b563316e409b2e9d23c553a746fcffb311
Thriveni-123 <71956386+Thriveni-123@users.noreply.github.com>
2021-01-27 16:58:07 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    defaultServerResponse : {
+        status : 205,
+        message : '',
+        body : {}
+    },
+    requestValidationMessage: {
+        BAD_REQUEST: 'Invalid fields'
+    },
+    USERMESSAGE:{
+      SEND:'Email send successfully.',
+      FETCH:"getting your details successfully"
+       
+    }
+}
